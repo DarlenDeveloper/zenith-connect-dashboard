e4d267df99d071aa6cf2be25498382a727c718da
DarlenDeveloper <darlendev@outlook.com>
2025-04-16 20:11:06 +0300

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
 // Environment variables
 
 // Stripe keys
-// export const STRIPE_PUBLISHABLE_KEY = "pk_test_51R8SCVPEXvlHYAZ39FBQrzQvnl5qIo3EbZyZ7zsiGW1WQhv3vQjukIa6IKkC2egwM9OptpBGWTiFzrlGwWpA4xLW00PFhoJKUE";
+// export const STRIPE_PUBLISHABLE_KEY = "";
 // Secret key is not stored here - it's handled server-side only
