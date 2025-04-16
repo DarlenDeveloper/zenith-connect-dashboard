54c4e60e874622a635740174c2ef25d479bef37b
DarlenDeveloper <darlendev@outlook.com>
2025-04-16 20:13:39 +0300

    Update ZenithLogo.tsx

--- src/components/ZenithLogo.tsx
+++ src/components/ZenithLogo.tsx
@@ -29,7 +29,7 @@ const ZenithLogo = ({ className }: ZenithLogoProps) => {
           </g>
         </g>
       </svg>
-      <span className="font-bold text-xl">Zenith</span>
+      <span className="font-bold text-xl">AIRIES AI</span>
     </div>
   );
 };
